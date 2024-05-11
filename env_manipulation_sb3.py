c10e1296da356ad05be26d7477a7c567d67f7c17
Julien Perez <perezjln@users.noreply.github.com>
2024-05-11 16:24:39 +0200

    feat: Add PPO algorithm and environment support
    This commit adds support for the PPO (Proximal Policy Optimization) algorithm and the `ReachCubeEnv` environment. The code now includes a new function `do_ppo()` that initializes the environment and trains the PPO agent. The `do_env_sim()` function has been renamed to `do_td3()` to reflect the change in algorithm. The commit also includes necessary imports and updates to the `env_manipulation_sb3.py` file.

--- env_manipulation_sb3.py
+++ env_manipulation_sb3.py
@@ -1,11 +1,11 @@
 
-from stable_baselines3 import TD3
-from stable_baselines3.common.vec_env import DummyVecEnv
+from stable_baselines3 import TD3, PPO
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from stable_baselines3.common.evaluation import evaluate_policy
 
 from envs.ReachCubeEnv import ReachCubeEnv
 
-def do_env_sim():
+def do_td3():
 
     do_render = False
     env = ReachCubeEnv(render=do_render, max_episode_steps=200)
@@ -18,5 +18,22 @@ def do_env_sim():
     mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
     print(f"Mean reward: {mean_reward} +/- {std_reward}")
 
+
+def do_ppo():
+
+    nb_parallel_env = 4
+    do_render = False
+    envs = SubprocVecEnv([lambda: ReachCubeEnv(render=do_render, max_episode_steps=200) for _ in range(nb_parallel_env)])
+
+    # Define and train the TD3 agent
+    model = PPO("MlpPolicy", envs, verbose=1)
+    model.learn(total_timesteps=int(1e5))
+
+    # Evaluate the agent
+    env = ReachCubeEnv(render=do_render, max_episode_steps=200)
+    mean_reward, std_reward = evaluate_policy(model, env, n_eval_episodes=10)
+    print(f"Mean reward: {mean_reward} +/- {std_reward}")
+
+
 if __name__ == '__main__':
-    do_env_sim()
+    do_ppo()
