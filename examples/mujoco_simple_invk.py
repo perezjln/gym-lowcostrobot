0cece8576a4f59858d4f22346c9d9c34c1c88771
Julien Perez <perezjln@users.noreply.github.com>
2024-05-23 20:49:17 +0200

    Add invk for the 6dof version of the lowcostrobot.

--- examples/mujoco_simple_invk.py
+++ examples/mujoco_simple_invk.py
@@ -52,6 +52,52 @@ def do_simple_invk_5dof():
 
 def do_simple_invk_6dof():
 
+    m = mujoco.MjModel.from_xml_path("gym_lowcostrobot/assets/low_cost_robot/scene.xml")
+    data = mujoco.MjData(m)
+    robot = SimulatedRobot(m, data)
+
+    with mujoco.viewer.launch_passive(m, data) as viewer:
+        # Get the final position of the cube
+        cube_pos = data.joint("red_box_joint").qpos[:3]
+
+        # Run the simulation
+        while viewer.is_running():
+            step_start = time.time()
+            q_target_pos = robot.inverse_kinematics(ee_target_pos=cube_pos, step=1.0, joint_name="moving_side", nb_dof=6)
+            robot.set_target_pos(q_target_pos)
+
+            # Step the simulation forward
+            mujoco.mj_step(m, data)
+            viewer.sync()
+
+            # Rudimentary time keeping, will drift relative to wall clock.
+            time_until_next_step = m.opt.timestep - (time.time() - step_start)
+            if time_until_next_step > 0:
+                time.sleep(time_until_next_step)
+
+            # Get the final position of the cube
+            cube_pos = data.joint("red_box_joint").qpos[:3]
+
+            ee_id = m.body("moving_side").id
+            ee_pos = data.geom_xpos[ee_id]
+
+            print("Cube dist:", np.linalg.norm(cube_pos - ee_pos))
+            if np.linalg.norm(cube_pos - ee_pos) < 0.1 or np.linalg.norm(cube_pos - ee_pos) > 0.13:
+                print("Cube reached the target position")
+
+                data.qpos[:6] = 0.0
+                data.joint("red_box_joint").qpos[:3] = [np.random.rand() * 0.2, np.random.rand() * 0.2, 0.01]
+                mujoco.mj_forward(m, data)
+                viewer.sync()
+
+                # Rudimentary time keeping, will drift relative to wall clock.
+                time_until_next_step = m.opt.timestep - (time.time() - step_start)
+                if time_until_next_step > 0:
+                    time.sleep(time_until_next_step)
+
+
+def do_simple_invk_6dof_soarm():
+
     m = mujoco.MjModel.from_xml_path("gym_lowcostrobot/assets/scene_so_arm_6dof_one_cube.xml")
     data = mujoco.MjData(m)
     robot = SimulatedRobot(m, data)
@@ -98,11 +144,13 @@ def do_simple_invk_6dof():
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Choose between 5dof and 6dof lowcost robot simulation.")
-    parser.add_argument("--robot", choices=["5dof", "6dof"], default="5dof", help="Choose the lowcost robot type")
+    parser.add_argument("--robot", choices=["5dof", "6dof", "6dof_soarm"], default="5dof", help="Choose the lowcost robot type")
     args = parser.parse_args()
 
     if args.robot == "5dof":
         do_simple_invk_5dof()
     elif args.robot == "6dof":
         do_simple_invk_6dof()
+    elif args.robot == "6dof_soarm":
+        do_simple_invk_6dof_soarm()
 
