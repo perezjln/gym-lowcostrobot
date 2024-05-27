2820cd63c03d4c4334ae665c0e2901e00b6c3974
Julien Perez <perezjln@users.noreply.github.com>
2024-05-28 00:55:22 +0200

    chore: Update HDF5 trace recording wrapper to use dynamic folder path

--- examples/trace_hdf5_record.py
+++ examples/trace_hdf5_record.py
@@ -11,7 +11,7 @@ from gym_lowcostrobot.envs.wrappers.record_hdf5 import RecordHDF5Wrapper
 def do_record_hdf5(args):
 
     env = ReachCubeEnv(render_mode=None, image_state="multi", action_mode="ee")
-    env = RecordHDF5Wrapper(env, hdf5_folder="data", length=1000, name_prefix="reach")
+    env = RecordHDF5Wrapper(env, hdf5_folder=args.folder, length=1000, name_prefix="reach")
     env.reset()
 
     max_step = 20
@@ -25,6 +25,6 @@ def do_record_hdf5(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Trace video from HDF5 trace file")
-    parser.add_argument("--file_path", type=str, default="data/episode_49.hdf5", help="Path to HDF5 file")
+    parser.add_argument("--folder", type=str, default="data/", help="Path to HDF5 folder")
     args = parser.parse_args()
     do_record_hdf5(args)
