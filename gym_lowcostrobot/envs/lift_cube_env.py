71f83b2f4410b6e4db1460ef81a892f81d68036a
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-06-08 14:55:29 +0000

    lift single file

--- gym_lowcostrobot/envs/lift_cube_env.py
+++ gym_lowcostrobot/envs/lift_cube_env.py
@@ -4,13 +4,13 @@ import gymnasium as gym
 import mujoco
 import mujoco.viewer
 import numpy as np
-from gymnasium import spaces
+from gymnasium import spaces, Env
 
 from gym_lowcostrobot import ASSETS_PATH
 from gym_lowcostrobot.envs.base_env import BaseRobotEnv
 
 
-class LiftCubeEnv(BaseRobotEnv):
+class LiftCubeEnv(Env):
     """
     ## Description
 
@@ -49,7 +49,7 @@ class LiftCubeEnv(BaseRobotEnv):
     - `"arm_qvel"`: the joint velocities of the robot arm in radians per second, shape (6,)
     - `"image_front"`: the front image of the camera of size (240, 320, 3)
     - `"image_top"`: the top image of the camera of size (240, 320, 3)
-    - `"object_qpos"`: the position of the cube, as (x, y, z)
+    - `"cube_pos"`: the position of the cube, as (x, y, z)
 
     Three observation modes are available: "image" (default), "state", and "both".
 
@@ -59,7 +59,7 @@ class LiftCubeEnv(BaseRobotEnv):
     | `"arm_qvel"`    | ✓         | ✓         | ✓        |
     | `"image_front"` | ✓         |           | ✓        |
     | `"image_top"`   | ✓         |           | ✓        |
-    | `"object_qpos"` |           | ✓         | ✓        |
+    | `"cube_pos"`    |           | ✓         | ✓        |
 
     ## Reward
 
@@ -67,43 +67,102 @@ class LiftCubeEnv(BaseRobotEnv):
     distance.
     """
 
-    def __init__(self, observation_mode="image", action_mode="joint", render_mode=None):
-        super().__init__(
-            xml_path=os.path.join(ASSETS_PATH, "scene_one_cube.xml"),
-            observation_mode=observation_mode,
-            action_mode=action_mode,
-            render_mode=render_mode,
-        )
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
 
-        # Define the action space and observation space
+    def __init__(self, observation_mode="image", action_mode="joint", render_mode=None):
+        # Load the MuJoCo model and data
+        self.model = mujoco.MjModel.from_xml_path(os.path.join(ASSETS_PATH, "lift_cube.xml"), {})
+        self.data = mujoco.MjData(self.model)
+
+        # Set the action space
+        self.action_mode = action_mode
         action_shape = {"joint": 6, "ee": 4}[action_mode]
         self.action_space = spaces.Box(low=-1.0, high=1.0, shape=(action_shape,), dtype=np.float32)
 
         # Set the observations space
+        self.observation_mode = observation_mode
         observation_subspaces = {
             "arm_qpos": spaces.Box(low=-np.pi, high=np.pi, shape=(6,)),
             "arm_qvel": spaces.Box(low=-10.0, high=10.0, shape=(6,)),
         }
-        if observation_mode in ["image", "both"]:
+        if self.observation_mode in ["image", "both"]:
             observation_subspaces["image_front"] = spaces.Box(0, 255, shape=(240, 320, 3), dtype=np.uint8)
             observation_subspaces["image_top"] = spaces.Box(0, 255, shape=(240, 320, 3), dtype=np.uint8)
-        if observation_mode in ["state", "both"]:
-            observation_subspaces["object_qpos"] = spaces.Box(low=-10.0, high=10.0, shape=(3,))
+            self.renderer = mujoco.Renderer(self.model)
+        if self.observation_mode in ["state", "both"]:
+            observation_subspaces["cube_pos"] = spaces.Box(low=-10.0, high=10.0, shape=(3,))
         self.observation_space = gym.spaces.Dict(observation_subspaces)
 
+        # Set the render utilities
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+        if self.render_mode == "human":
+            self.viewer = mujoco.viewer.launch_passive(self.model, self.data)
+
+        # Set additional utils
         self.threshold_height = 0.5
-        self.object_low = np.array([-0.15, -0.15, 0.05])
-        self.object_high = np.array([0.15, 0.15, 0.05])
+        self.object_low = np.array([-0.15, 0.05, 0.025])
+        self.object_high = np.array([0.15, 0.15, 0.025])
+
+
+    def apply_action(self, action):
+        """
+        Step the simulation forward based on the action
+
+        Action shape
+        - EE mode: [dx, dy, dz, gripper]
+        - EE mode and block gripper: [dx, dy, dz]
+        - Joint mode: [q1, q2, q3, q4, q5, q6, gripper]
+        - Joint mode and block gripper: [q1, q2, q3, q4, q5, q6]
+        """
+        if self.action_mode == "ee":
+            ee_action, gripper_action = action[:3], action[-1]
+
+            # Update the robot position based on the action
+            ee_id = self.model.body("moving_side").id
+            ee_target_pos = self.data.xpos[ee_id] + ee_action
+
+            # Use inverse kinematics to get the joint action wrt the end effector current position and displacement
+            target_qpos = self.inverse_kinematics(ee_target_pos=ee_target_pos)
+            target_qpos[-1:] = gripper_action
+        elif self.action_mode == "joint":
+            target_qpos = action
+        else:
+            raise ValueError("Invalid action mode, must be 'ee' or 'joint'")
+
+        # Set the target position
+        self.data.ctrl = target_qpos
+
+        # Step the simulation forward
+        mujoco.mj_step(self.model, self.data)
+        if self.render_mode == "human":
+            self.viewer.sync()
+
+
+    def get_observation(self):
+        observation = {
+            "arm_qpos": self.data.qpos[:6].astype(np.float32),
+            "arm_qvel": self.data.qvel[:6].astype(np.float32),
+        }
+        if self.observation_mode in ["image", "both"]:
+            self.renderer.update_scene(self.data, camera="camera_front")
+            observation["image_front"] = self.renderer.render()
+            self.renderer.update_scene(self.data, camera="camera_top")
+            observation["image_top"] = self.renderer.render()
+        if self.observation_mode in ["state", "both"]:
+            observation["cube_pos"] = self.data.qpos[6:9].astype(np.float32)
+        return observation
+
 
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
         super().reset(seed=seed, options=options)
 
         # Reset the robot to the initial position and sample the object position
-        robot_qpos = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
         object_pos = self.np_random.uniform(self.object_low, self.object_high)
         object_rot = np.array([1.0, 0.0, 0.0, 0.0])
-        self.data.qpos[:13] = np.concatenate([robot_qpos, object_pos, object_rot])
+        robot_qpos = np.array([0.0, 0.0, 0.0, 0.0, 0.0, 0.0])
+        self.data.qpos[:] = np.concatenate([object_pos, object_rot, robot_qpos])
 
         # Step the simulation
         mujoco.mj_forward(self.model, self.data)
@@ -112,19 +171,28 @@ class LiftCubeEnv(BaseRobotEnv):
 
     def step(self, action):
         # Perform the action and step the simulation
-        self.apply_action(action, block_gripper=False)
+        self.apply_action(action)
 
         # Get the new observation
         observation = self.get_observation()
 
         # Get the height of the object
-        cube_id = self.model.body("box").id
-        object_z = self.data.geom_xpos[cube_id][2]
+        cube_z = self.data.qpos[8]
 
         # Compute the reward
-        reward = object_z
+        reward = cube_z
 
         # Check if the target position is reached
-        terminated = object_z > self.threshold_height
+        terminated = cube_z > self.threshold_height
 
         return observation, reward, terminated, False, {}
+
+    def render(self):
+        if self.render_mode == "human":
+            self.viewer.sync()
+
+    def close(self):
+        if self.render_mode == "human":
+            self.viewer.close()
+        if self.observation_mode in ["image", "both"]:
+            self.renderer.close()
