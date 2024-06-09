2718095748f8aeb20cebe0e097f097741a581f70
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-06-09 14:46:24 +0000

    uniformize max steps

--- gym_lowcostrobot/__init__.py
+++ gym_lowcostrobot/__init__.py
@@ -9,29 +9,29 @@ ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets/low_cost_robot_6do
 register(
     id="LiftCube-v0",
     entry_point="gym_lowcostrobot.envs:LiftCubeEnv",
-    max_episode_steps=200,
+    max_episode_steps=500,
 )
 
 register(
     id="PickPlaceCube-v0",
     entry_point="gym_lowcostrobot.envs:PickPlaceCubeEnv",
-    max_episode_steps=200,
+    max_episode_steps=500,
 )
 
 register(
     id="PushCube-v0",
     entry_point="gym_lowcostrobot.envs:PushCubeEnv",
-    max_episode_steps=200,
+    max_episode_steps=500,
 )
 
 register(
     id="ReachCube-v0",
     entry_point="gym_lowcostrobot.envs:ReachCubeEnv",
-    max_episode_steps=200,
+    max_episode_steps=500,
 )
 
 register(
     id="Stack-v0",
     entry_point="gym_lowcostrobot.envs:StackEnv",
-    max_episode_steps=200,
+    max_episode_steps=500,
 )
