762b5d2545583aec89de9e571ea7bfecb9cd97dd
Quentin Gallouédec <quentin.gallouedec@huggingface.co>
2024-06-10 11:04:40 +0000

    image test

--- tests/test_env.py
+++ tests/test_env.py
@@ -6,7 +6,7 @@ import gym_lowcostrobot  # noqa
 
 
 @pytest.mark.parametrize("env_id", ["LiftCube-v0", "PickPlaceCube-v0", "PushCube-v0", "ReachCube-v0", "StackTwoCubes-v0"])
-@pytest.mark.parametrize("observation_mode", ["state"])
+@pytest.mark.parametrize("observation_mode", ["state", "image"])
 def test_env_check(env_id, observation_mode):
     env = gym.make(env_id, observation_mode=observation_mode)
     check_env(env, skip_render_check=True)
