46e79f0cf671ffea315b485152d0878b14226476
Kevin Rohling <kevin@kevinrohling.com>
2024-09-19 11:26:57 -0700

    Adding MANIFEST.in file and updating setup.py to include the assets directory when the project is pip installed.

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(
     author="Julien Perez",
     author_email="julien.perez@epita.fr",
     packages=find_packages(),
+    include_package_data=True,
     install_requires=["gymnasium>=0.29", "mujoco>=3.0", "PyOpenGL==3.1.1a1"],
 )
