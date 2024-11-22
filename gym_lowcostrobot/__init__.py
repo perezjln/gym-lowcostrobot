d5a2eda11a6931878afbe154f0f2191aaaf2a12c
lilkm <meftah.khalil@live.fr>
2024-11-22 18:06:54 +0100

    change max_episode_steps to 50 steps

--- gym_lowcostrobot/__init__.py
+++ gym_lowcostrobot/__init__.py
@@ -10,35 +10,35 @@ BASE_LINK_NAME = "link_1"
 register(
     id="LiftCube-v0",
     entry_point="gym_lowcostrobot.envs:LiftCubeEnv",
-    max_episode_steps=500,
+    max_episode_steps=50,
 )
 
 register(
     id="PickPlaceCube-v0",
     entry_point="gym_lowcostrobot.envs:PickPlaceCubeEnv",
-    max_episode_steps=500,
+    max_episode_steps=50,
 )
 
 register(
     id="PushCube-v0",
     entry_point="gym_lowcostrobot.envs:PushCubeEnv",
-    max_episode_steps=500,
+    max_episode_steps=50,
 )
 
 register(
     id="ReachCube-v0",
     entry_point="gym_lowcostrobot.envs:ReachCubeEnv",
-    max_episode_steps=500,
+    max_episode_steps=50,
 )
 
 register(
     id="StackTwoCubes-v0",
     entry_point="gym_lowcostrobot.envs:StackTwoCubesEnv",
-    max_episode_steps=500,
+    max_episode_steps=50,
 )
 
 register(
     id="PushCubeLoop-v0",
     entry_point="gym_lowcostrobot.envs:PushCubeLoopEnv",
-    max_episode_steps=500,
+    max_episode_steps=50,
 )
