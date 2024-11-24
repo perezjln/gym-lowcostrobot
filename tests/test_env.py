a9077a33c18ac09c0484ada60d9b01c44895c7fd
lilkm <meftah.khalil@live.fr>
2024-11-24 23:01:21 +0100

    add PushCubeLoop env to test_env.py script

--- tests/test_env.py
+++ tests/test_env.py
@@ -5,7 +5,7 @@ from gymnasium.utils.env_checker import check_env
 import gym_lowcostrobot  # noqa
 
 
-@pytest.mark.parametrize("env_id", ["LiftCube-v0", "PickPlaceCube-v0", "PushCube-v0", "ReachCube-v0", "StackTwoCubes-v0"])
+@pytest.mark.parametrize("env_id", ["LiftCube-v0", "PickPlaceCube-v0", "PushCube-v0", "ReachCube-v0", "StackTwoCubes-v0", "PushCubeLoop-v0"])
 @pytest.mark.parametrize("observation_mode", ["state", "image"])
 def test_env_check(env_id, observation_mode):
     env = gym.make(env_id, observation_mode=observation_mode)
